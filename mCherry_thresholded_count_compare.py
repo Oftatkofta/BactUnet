9e4ab14043f2d040e475b79a933c579b51a209c7
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2022-10-19 08:47:56 +0200

    quantification work

---
+++ mCherry_thresholded_count_compare.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import tifffile
+from tifffile import TiffFile
+from skimage.filters import threshold_otsu
+from skimage.morphology import binary_erosion, binary_dilation
+from skimage.measure import find_contours
+from skimage.restoration import rolling_ball, ellipsoid_kernel
+from skimage.filters import gaussian
+
+import os
+
+def apply_rolling_ball(frame):
+    background = rolling_ball(frame, kernel=kernel)
+    filtered_image = frame - background
+    return filtered_image
+
+def bg_subtract(arr):
+    bg_arr = []
+    for i in range(len(arr)):
+        print("rolling with frame: ", i)
+        a = gaussian(arr[i], sigma=0.8, preserve_range=True)
+        a = apply_rolling_ball(a)
+        bg_arr.append(a)
+    bg_arr = (bg_arr/np.max(bg_arr))*255
+
+    return bg_arr.astype('uint8')
+
+def count_bacteria(arr):
+
+    bact_per_frame = []
+
+    for frame in arr:
+        nbact = 0
+        contours = find_contours(frame)
+        for c in contours:
+            if c.size > 100:
+                nbact += 1
+        bact_per_frame.append(nbact)
+
+    return bact_per_frame
+
+def threshold_array(arr):
+    out = np.empty_like(arr)
+
+    for i in range(len(arr)):
+        frame = arr[i]
+        th = threshold_otsu(frame)
+        frame = frame > th*1.5
+        frame = binary_erosion(frame)
+        frame = binary_erosion(frame)
+        frame = binary_dilation(frame)
+
+        out[i] = frame
+    out = out * 255
+
+    return out.astype('uint8')
+
+
+infiles = {
+        "BT0398_Ch2":r"C:\Users\Jens\Documents\Code\BactUnet\Bactnet\BT0398_OGM\C2-anodisc_OGM_1_MMStack_Default.ome.tif",
+        "BT402_Ch2":r"C:\Users\Jens\Documents\Code\BactUnet\Bactnet\BT402_ENR_wt\C2-BT0402-anodisc_ENR_wt_1_MMStack_Default.tif",
+        "BT403_Ch2":r"C:\Users\Jens\Documents\Code\BactUnet\Bactnet\BT403_OGM_wt\C2-anodisc_OGM_wt_1_MMStack_Default.ome.tif",
+        "BT404_Ch2":r"C:\Users\Jens\Documents\Code\BactUnet\Bactnet\BT404_ENRRT_wt\C2_anodisc_ENR+RT_wt_1_MMStack_Default_1.ome.tif"
+            }
+
+outdir = r"C:\Users\Jens\Documents\Code\BactUnet\Bactnet\mcherry_count_temp"
+
+kernel = ellipsoid_kernel((25, 25), 75)
+counts = None
+
+for k in infiles.keys():
+    with TiffFile(infiles[k]) as tif:
+        arr =  tif.asarray()[1:-1,:,:]
+    bg_arr = bg_subtract(arr)
+    bg_arr = threshold_array(bg_arr)
+    raw_nbact = count_bacteria(bg_arr)
+    tifffile.imwrite(os.path.join(outdir, k+".tif"), bg_arr)
+    df = pd.DataFrame({k:raw_nbact})
+    if counts is None:
+        counts = df
+    else:
+        counts = pd.concat((counts,df), axis=1)
+counts.to_csv(os.path.join(outdir,"counts.csv"), index=False)
--- quantification_visualization.py
+++ quantification_visualization.py
@@ -7,33 +7,50 @@ Created on Tue Jun 28 16:21:01 2022
 import pandas as pd
 import seaborn as sns
 import os
-
-from matplotlib import pyplot as plt
+import numpy as np
 
 startpath = r"F:\BactUnet"
-infile = os.path.join(startpath, 'fl_quantifiaction_raw_data.csv')
+infile = os.path.join(startpath, 'bacteria_count_bactunet_V3.csv')
 alldata = pd.read_csv(infile)
 onlywt = alldata[alldata.bacteria == 'wt']
 
-infile = os.path.join(startpath,  "fl_quantifiaction_otsu_data.csv")
-otsudata = pd.read_csv(infile)
-otsuwt = otsudata[otsudata.bacteria == 'wt']
 
-sns.set_theme(style="ticks")
+def hampel(vals_orig, k=7, t0=3):
+    '''
+    vals: pandas series of values from which to remove outliers
+    k: size of window (including the sample; 7 is equal to 3 on either side of value)
+    '''
+    #Make copy so original not edited
+    vals=vals_orig.copy()    
+    #Hampel Filter
+    L= 1.4826
+    rolling_median=vals.rolling(k).median()
+    difference=np.abs(rolling_median-vals)
+    median_abs_deviation=difference.rolling(k).median()
+    threshold= t0 *L * median_abs_deviation
+    outlier_idx=difference>threshold
+    vals[outlier_idx]=np.nan
+    return(vals)
 
-g = sns.FacetGrid(col_wrap=4, col="filename", hue="condition", sharey=False, data=otsuwt)
-g.map(sns.lineplot, "frame", "thresholded_median_intensity", alpha=.7)
-#g.add_legend()
+g1 = sns.relplot(data=onlywt, x='frame', y='count', kind='line', hue='condition', col='method')
+g1.set(ylim=(0,200))
 
-#sns.relplot(x="frame", y="norm_median_intensity", col="experiment", hue="condition", data=onlywt,
-#           col_wrap=4, ci=None, palette="muted", height=4, sharex=False)
+g1 = sns.relplot(data=onlywt, x='frame', y='count', kind='line', hue='filename', row='method', col='condition')
+g1.set(ylim=(0,200))
 
+no_outliers = None
 
-
-# =============================================================================
-# sns.relplot(x="frame", y="raw_mean_intensity", hue="condition",
-#             kind="line", 
-#             data=onlywt)
-# =============================================================================
-plt.ylim(0, 0.4)
-plt.show()
+for m in onlywt.method.unique():
+    sf = onlywt[onlywt['method']==m]
+    for fn in onlywt.filename.unique():
+        ssf = sf[(sf['filename']==fn)]
+        cnts = ssf['count']
+        ssf['count_hampel'] = hampel(cnts, 7, 5)
+        if no_outliers is None:
+            no_outliers = ssf
+        no_outliers = pd.concat((no_outliers, ssf), ignore_index=True)
+ 
+   
+ 
+g2 = sns.relplot(data=no_outliers.dropna(), x='frame', y='count_hampel', kind='line', hue='condition', col='method')
+g2.set(ylim=(0,200))
