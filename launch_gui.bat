a63b1ccf7bcb3f777140eeb1a9c9bc1e7dfb2627
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2024-01-31 13:19:10 +0100

    Update launch_gui.bat

--- launch_gui.bat
+++ launch_gui.bat
@@ -1,5 +1,8 @@
 @echo off
-CALL C:\Users\YourUsername\miniconda3\Scripts\activate.bat YourCondaEnvironmentName
-SET PATH=%PATH%;C:\Some\Additional\Path
-python C:\Path\To\YourScript\your_script.py
+CALL C:\Users\analyst\Anaconda3\Scripts\activate.bat tf
+SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\bin;%PATH%
+SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\extras\CUPTI\lib64;%PATH%
+SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\include;%PATH%
+SET PATH=C:\tools\cuda\bin;%PATH%
+python "C:\Users\analyst\Documents\Python Scripts\BactUnet\bactunet_gui.py"
 pause
